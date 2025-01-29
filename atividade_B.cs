ba84d3b24adc40da245847e1873639fa79c73a8b
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2025-01-29 07:44:14 -0300

    atividade_B.cs

--- atividade_B.cs
+++ atividade_B.cs
@@ -9,9 +9,9 @@ namespace att01
 			Console.Write("Insira um numero: ");
 			float numero = Convert.ToSingle(Console.ReadLine());
 
-			float soma = numero * 2;
+			float dobro = numero * 2;
 			
-			Console.WriteLine("\r\nA soma dos dois numeros é: " + soma);
+			Console.WriteLine("\r\nA soma dos dois numeros é: " + dobro);
 			
 			Console.WriteLine("\r\nAperte qualquer tecla para continuar...");
 			Console.ReadKey(true);
