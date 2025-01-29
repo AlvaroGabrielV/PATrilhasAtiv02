5c74f842abb5467d99dfef10c492c7e7db57ec57
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2025-01-29 07:46:42 -0300

    atividade_D.cs

--- atividade_D.cs
+++ atividade_D.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace att01
+{
+	class Program
+	{
+		public static void Main()
+		{	
+			Console.Write("Insira um numero: ");
+			float numero = Convert.ToSingle(Console.ReadLine());
+
+			float quadrado = numero * numero;
+			
+			Console.WriteLine("\r\nA soma dos dois numeros é: " + quadrado);
+			
+			Console.WriteLine("\r\nAperte qualquer tecla para continuar...");
+			Console.ReadKey(true);
+		}
+	}
+}
